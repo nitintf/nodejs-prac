5c240e71ae6cfcc37133c6f9f31bb744fc1af33c
Nitin Panwar <nitinpanwar0802@gmail.com>
2022-01-04 13:08:30 +0530

    refactor: using monogdb CRUD
    using mongodb CRUD operations

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,18 +13,16 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
+  const product = new Product(title, price, description, imageUrl)
 
-  req.user.createProduct({
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description,
-  }).then((result) => {
-    console.log('Created Product');
-    res.redirect('/admin/products')
-  }).catch(error => {
-    console.log('error :>> ', error);
-  })
+  product
+    .save()
+    .then((result) => {
+      console.log('Created Product');
+      res.redirect('/admin/products')
+    }).catch(error => {
+      console.log('error :>> ', error);
+    })
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -33,16 +31,13 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  req.user.getProducts({
-    where: {
-      id: prodId
-    }
-  }).then(products => {
+
+  Product.findById(prodId).then(product => {
     res.render('admin/edit-product', {
       pageTitle: 'Edit Product',
       path: '/admin/edit-product',
       editing: editMode,
-      product: products[0]
+      product: product
     });
   }).catch(error => {
     console.log('error :>> ', error);
@@ -57,15 +52,11 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
 
-  Product.findByPk(prodId)
+  const product = new Product(updatedTitle, updatedPrice, updatedDesc, updatedImageUrl, prodId)
+
+  product
+    .save()
     .then(product => {
-      product.title = updatedTitle
-      product.price = updatedPrice
-      product.description = updatedDesc
-      product.imageUrl = updatedImageUrl
-      return product.save()
-    })
-    .then(result => {
       console.log('UPDATED PRODUCT');
       res.redirect('/admin/products')
     })
@@ -75,25 +66,21 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  req.user.getProducts()
+  Product.fetchAll()
     .then(products => {
       res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
         path: '/admin/products'
-      }).catch(error => {
-        console.log('error :>> ', error);
-      });
-    });
-
+      })
+    }).catch(error => {
+      console.log('error :>> ', error);
+    });;
 };
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByPk(prodId)
-    .then(product => {
-      return product.destroy()
-    })
+  Product.deleteById(prodId)
     .then(result => {
       console.log('PRODUCT DELETED');
       res.redirect('/admin/products');
