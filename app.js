8e3c5be944948966433ae3bc1bd7378ef549bd43
Nitin Panwar <nitinpanwar0802@gmail.com>
2022-01-04 13:04:17 +0530

    feat: init mongodb

--- app.js
+++ app.js
@@ -4,13 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const sequelize = require('./util/database');
-const Product = require('./models/product')
-const User = require('./models/user')
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
+const mongoConnect = require('./util/database').mongoConnect
 
 const app = express();
 
@@ -24,12 +18,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findByPk(1).then((user) => {
-    req.user = user;
-    next()
-  }).catch(err => {
-    console.log('err :>> ', err);
-  })
+  // User.findByPk(1).then((user) => {
+  //   req.user = user;
+  //   next()
+  // }).catch(err => {
+  //   console.log('err :>> ', err);
+  // })
+  next()
 })
 
 app.use('/admin', adminRoutes);
@@ -37,36 +32,8 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-// DB ASSOCIATIONS
-Product.belongsTo(User, {
-  constraints: true, onDelete: 'CASCADE'
-});
-User.hasMany(Product) // Same above
-User.hasOne(Cart)
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem })
-
-sequelize
-  .sync()
-  .then(result => {
-    return User.findByPk(1)
+mongoConnect(() => {
+  app.listen(3000, () => {
+    console.log('App running at port: 3000');
   })
-  .then((user) => {
-    if (!user) {
-      User.create({ name: 'Nitin', email: 'nitinpanwar@gmail.com' });
-    }
-
-    return user;
-  })
-  .then((user) => {
-    // return user.createCart()
-  }).then(cart => {
-    app.listen(3000);
-  })
-  .catch(error => {
-    console.log('err :>> ', error);
-  })
+})
