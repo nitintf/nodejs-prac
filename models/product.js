629280a93b09f6da60a4a84867f2b1ace54dc040
Nitin Panwar <nitinpanwar0802@gmail.com>
2022-01-04 13:10:02 +0530

    refactor: Product class
    created product class Model using mongodb

--- models/product.js
+++ models/product.js
@@ -1,27 +1,78 @@
-const Sequelize = require('sequelize');
+const getDb = require('../util/database').getDb;
+const mongodb = require('mongodb')
+class Product {
+  constructor(title, price, description, imageUrl, id) {
+    this.title = title
+    this.price = price
+    this.description = description
+    this.imageUrl = imageUrl
+    this._id = id ? new mongodb.ObjectID(id) : null
+  }
 
-const sequelize = require('../util/database');
+  save() {
+    const db = getDb();
+    let dbOp;
+    if (this._id) {
+      dbOp = db
+        .collection('product')
+        .updateOne({ _id: this._id }, {
+          $set: this
+        })
+    } else {
+      dbOp = db
+        .collection('product')
+        .insertOne(this)
+    }
 
-const Product = sequelize.define('product', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-});
+    return dbOp
+      .then(result => {
+        console.log('result :>> ', result);
+      })
+      .catch(err => {
+        console.log('err :>> ', err);
+      })
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection('product')
+      .find()
+      .toArray()
+      .then(products => {
+        return products
+      })
+      .catch(err => {
+        console.log('err :>> ', err);
+      });
+  }
+
+  static findById(id) {
+    const db = getDb()
+    return db
+      .collection('product')
+      .find({ _id: new mongodb.ObjectID(id) })
+      .next()
+      .then(product => {
+        return product
+      })
+      .catch(err => {
+        console.log('err :>> ', err);
+      })
+  }
+
+  static deleteById(id) {
+    const db = getDb()
+    return db
+      .collection('product')
+      .deleteMany({ _id: new mongodb.ObjectID(id) })
+      .then(response => {
+        return response
+      })
+      .catch(err => {
+        console.log('err :>> ', err);
+      })
+  }
+}
 
 module.exports = Product;
