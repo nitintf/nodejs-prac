cfb7e9eb3a90a1fd500a6050619849acf48532ef
Nitin Panwar <nitinpanwar0802@gmail.com>
2022-01-04 13:07:13 +0530

    feat: connect mongodb
    function to connect to mongodb

--- util/database.js
+++ util/database.js
@@ -1,8 +1,28 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb')
+const mongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-prac', 'root', 'Shadow@0802', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+  mongoClient
+    .connect('mongodb+srv://nitin:Shadow%400802@nodejs-prac.9z3hb.mongodb.net/shop?retryWrites=true&w=majority')
+    .then(result => {
+      console.log('DB Connected');
+      _db = result.db()
+      callback()
+    })
+    .catch(err => {
+      console.log('err :>> ', err);
+      throw err;
+    })
+}
+
+const getDb = () => {
+  if (_db) {
+    return _db
+  }
+  throw new Error('No database found')
+}
+
+exports.mongoConnect = mongoConnect
+exports.getDb = getDb
